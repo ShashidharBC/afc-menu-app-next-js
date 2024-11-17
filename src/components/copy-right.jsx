635be33ac133df0f1a4789d4c37fe4be19357c2a
ShashidharBC98 <shashidharbc.1dt17is079@gmail.com>
2024-11-17 11:51:49 +0530

    added the all rights reserved text in the next line

--- src/components/copy-right.jsx
+++ src/components/copy-right.jsx
@@ -4,14 +4,18 @@ const Copyright = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <div className="flex items-center justify-center pt-8 text-sm">
-      <div className="flex items-center space-x-1">
-        <span className="text-gray-600">©</span>
-        <span className="font-medium text-gray-800">{currentYear}</span>
-        <span className="text-gray-600 font-medium">
-          Shashidhara B Challamarada.
-        </span>
-        <span className="text-gray-600">All rights reserved.</span>
+    <div className="fixed bottom-0 left-0 lg:left-[12%] right-0 w-full bg-white border-t border-gray-200">
+      <div className="flex flex-col items-center justify-center py-4 text-sm">
+        <div className="flex items-center space-x-1">
+          <span className="text-gray-600">©</span>
+          <span className="font-medium text-gray-800">{currentYear}</span>
+          <span className="text-gray-600 font-medium">
+            Shashidhara B Challamarada.
+          </span>
+        </div>
+        <div className="mt-1">
+          <span className="text-gray-600">All rights reserved.</span>
+        </div>
       </div>
     </div>
   );
